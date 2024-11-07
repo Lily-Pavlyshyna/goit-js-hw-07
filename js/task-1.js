a4d56835649dd70e18c11f664a25ffa89243c040
Lily Pavlyshyna <pavlisinalilia869@gmail.com>
2024-11-07 15:41:02 +0200

    update files

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+// const categoriesList = document.querySelector("#categories");
+// const categoriesItem = document.querySelectorAll(".item");
+// console.log("Number of categories:", categoriesItem.length);
+// categoriesItem.forEach((item) => {
+//   const title = item.querySelector("h2").textContent;
+//   const el = item.querySelectorAll("li").length;
+//   console.log("Category:", title);
+//   console.log("Elements:", el);
+// });
